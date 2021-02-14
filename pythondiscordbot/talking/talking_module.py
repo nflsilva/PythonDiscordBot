2ab495fbe4a80aa7d24b5480187ad039018625b3
nflsilva <nelsonflsilva@gmail.com>
2021-02-14 13:16:05 +0000

    Added more functionality to TalkingModule

--- pythondiscordbot/talking/talking_module.py
+++ pythondiscordbot/talking/talking_module.py
@@ -1,5 +1,15 @@
+import random
+
 
 class NoPhrasesLeftException(Exception):
+    pass
+
+
+class PhraseNotFoundException(Exception):
+    pass
+
+
+class PhreaseAlreadyExistsException(Exception):
     pass
 
 
@@ -8,11 +18,32 @@ class TalkingModule():
     def __init__(self):
         self._phrases = []
 
+    def _validateId(self, phrase_id):
+        if phrase_id < 0 or phrase_id > len(self._phrases) - 1:
+            raise PhraseNotFoundException(
+                f"There are no phrases with id: {phrase_id}")
+
+    def number_of_phrases(self):
+        return len(self._phrases)
+
     def put_phrase(self, phrase):
+
+        if phrase in self._phrases:
+            raise PhreaseAlreadyExistsException
+
         self._phrases.append(phrase)
 
     def get_phrase(self):
         if len(self._phrases) > 0:
-            return self._phrases[(len(self._phrases) - 1)]
+            phrase_index = random.randint(0, len(self._phrases)-1)
+            return self._phrases[phrase_index]
         else:
             raise NoPhrasesLeftException("There are no phrases left.")
+
+    def update_phrase(self, phrase_id, new_phrase):
+        self._validateId(phrase_id)
+        self._phrases[phrase_id] = new_phrase
+
+    def remove_phrase(self, phrase_id):
+        self._validateId(phrase_id)
+        self._phrases.pop(phrase_id)
--- tests/test_talkingModule.py
+++ tests/test_talkingModule.py
@@ -1,6 +1,10 @@
 import unittest
 
-from pythondiscordbot.talking.talking_module import TalkingModule, NoPhrasesLeftException
+from pythondiscordbot.talking.talking_module import \
+    TalkingModule, \
+    NoPhrasesLeftException, \
+    PhraseNotFoundException, \
+    PhreaseAlreadyExistsException
 
 
 class TestTalkingModule(unittest.TestCase):
@@ -15,12 +19,76 @@ class TestTalkingModule(unittest.TestCase):
 
         self.assertEqual(new_phrase, stored_phrase)
 
+    def test_create_duplicated(self):
+
+        sut = TalkingModule()
+        new_phrase = "This is a new phrase. Will be duplicated"
+
+        sut.put_phrase(new_phrase)
+
+        try:
+            sut.put_phrase(new_phrase)
+            self.fail(
+                "Method did not throw Exception when there are duplicated phrases.")
+        except PhreaseAlreadyExistsException:
+            pass
+
     def test_get_phrase_error(self):
 
         sut = TalkingModule()
 
         try:
             sut.get_phrase()
-            self.fail()
+            self.fail(
+                "Method did not throw Exception when there are no phrases stored.")
         except NoPhrasesLeftException:
             pass
+
+    def test_update_phrase(self):
+
+        original_phrase = "Original"
+        new_phrase = "Updated"
+        sut = TalkingModule()
+
+        sut.put_phrase(original_phrase)
+        sut.update_phrase(0, new_phrase)
+        updated_phrase = sut.get_phrase()
+
+        self.assertEquals(new_phrase, updated_phrase)
+
+    def test_update_phrase_error(self):
+
+        impossible_ids = [-1, 1, 5.9]
+        sut = TalkingModule()
+
+        for id in impossible_ids:
+
+            try:
+                sut.update_phrase(id, "Updated")
+            except PhraseNotFoundException:
+                continue
+
+            self.fail(f"Method did not throw Exception with invalid id {id}.")
+
+    def test_delete_phrase(self):
+
+        sut = TalkingModule()
+        new_phrase = "This is a new test phrase"
+
+        sut.put_phrase(new_phrase)
+        sut.remove_phrase(0)
+
+        self.assertEquals(sut.number_of_phrases(), 0)
+
+    def test_delete_phrase_error(self):
+
+        impossible_ids = [-1, 1, 5.9]
+        sut = TalkingModule()
+
+        for id in impossible_ids:
+            try:
+                sut.remove_phrase(id)
+            except PhraseNotFoundException:
+                continue
+
+            self.fail(f"Method did not throw Exception with invalid id {id}.")
